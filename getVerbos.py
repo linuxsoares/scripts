df35f36bf3c4db1889a86b42eb85bb9ca3e217cf
linuxsoares <linux.soares@gmail.com>
2013-11-26 20:44:15 -0200

    Enfim

--- getVerbos.py
+++ getVerbos.py
@@ -5,32 +5,60 @@ from bs4 import BeautifulSoup
 import codecs
 import MySQLdb
 import types
-
+import re
+from smtplib import SMTP
+from smtplib import SMTPException
+from email.mime.text import MIMEText
 from unicodedata import normalize 
 
 #'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
-arrayLetras = ('z')
+arrayLetras = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+
+def enviaEmail(texto):
+    sender = 'linux.soares@gmail.com'
+    receivers = ['linux.soares@gmail.com', 'pgluciano@gmail.com', 'gilmar.soares@autopass.com.br']
+    subject = 'Verbos - ConjugVerbos'
+    texto = texto.upper()
+    
+    message= """
+        Titulo: %s
+
+        Mensagem: %s
+    """ % (subject, texto)
+
+    try:
+        session = SMTP('smtp.gmail.com',587)
+        session.ehlo()
+        session.starttls()
+        session.ehlo()
+        session.login(sender,'password')
+        session.sendmail(sender,receivers,message)
+        session.quit()      
+    except SMTPException as e:
+        print "Error: unable to send email" + e.message
 
 def remover_acentos(txt, codif='utf-8'):
     return normalize('NFKD', txt.decode(codif)).encode('ASCII','ignore')
 
 def usarReplace(palavra):
-    return palavra.replace('não', '').replace('para ', '').replace('eu','').replace('voc','').replace(' ele/ela','').replace('vocs','').replace('tu','').replace('vs','').replace('no','').replace('ns','').replace('eles/elas','').replace('ele/ela','').replace(' s','').replace(' ','').replace('mês', '').replace('vós', '').replace('nós', '')
+    palavra = palavra.replace('não', '').replace('para ', '').replace('eu','').replace('voc','').replace(' ele/ela','').replace('vocs','').replace('tu','').replace('vs','').replace('no','').replace('ns','').replace('eles/elas','').replace('ele/ela','').replace(' s','').replace(' ','').replace('ês', '').replace('vós', '').replace('nós', '')
+    pattern = re.compile('(ê)$')
+    return  re.sub(pattern, '', palavra)
 
 
 def getConvertObjectSoup(url, codin='utf-8'):
     f = urllib.urlopen(url)
 
     html_doc = f.read()
-	
+        
     return BeautifulSoup(html_doc, from_encoding=codin)
 
 def getVerbos():
     verbos = file('verbos.txt', 'w')
     palavras = []
-	 
+         
     try:
-	 
+         
         for i in arrayLetras:
             url = 'http://www.achando.info/verbos/conjugar/dicionario-'+i+'.html'
 
@@ -39,28 +67,30 @@ def getVerbos():
             print i
             if soup.find_all('li') == []:
                 for s in soup.find_all('td', text=True):
-					#+ ' - ' + s.string
+                                        #+ ' - ' + s.string
                     if s.string and s.string != None and s.string != '':
                         #print s.string
                         palavras.append(s.string.encode('utf-8')  + '\n')
             else:
                 for a in soup.find_all('li'):             
                     soup = getConvertObjectSoup(a.find('a')['href'])
-					
-					#print soup
-					#verbos.write(url.encode('ascii', 'ignore') + '\n')
+                                        
+                                        #print soup
+                                        #verbos.write(url.encode('ascii', 'ignore') + '\n')
                     for s in soup.find_all('td', text=True):
-						#+ ' - ' + s.string
+                                                #+ ' - ' + s.string
                         if s.string and s.string != None and s.string != '':
-							#print s.string.encode('utf-8')
+                                                        #print s.string.encode('utf-8')
                             palavras.append(s.string.encode('utf-8')  + '\n')
-	                                  
-	                                      	                                                                                                         
+                                          
+                                                                                                                                                               
         verbos.writelines(palavras)
         verbos.close()
         return palavras
-    except Exception:
-        print 'Erro ao pegar verbos' + e.message
+    except Exception, e:
+        erro = 'Erro ao executar getVerbos verbos: ' + e.message
+        enviaEmail(erro)
+        print erro
     else:
         pass
     finally:
@@ -81,7 +111,7 @@ def validaVerbos(lista):
                     print 'Verbo ' + i.strip()+' Inexistente!! Delete BD'
                     lista.remove(i)
                 else:
-				    #print (soup.find(id="box1").h4.string)
+                                    #print (soup.find(id="box1").h4.string)
                     #print 'Verbo '+ str(i.strip()) + ' Ok na base do Aurelio'
                     print 'Insert '+str(i.strip())
                     sql = 'insert into verbos (nome) values ("'+ str(i.strip()) + '")'
@@ -93,12 +123,14 @@ def validaVerbos(lista):
         return sorted(lista)                
 
     except Exception, e:
-        print 'Erro ao verificar verbos.' + e.message
+        erro = 'Erro ao executar validaVerbos verbos: ' + e.message
+        enviaEmail(erro)
+        print erro
     else:
         pass
     finally:
         pass
-		
+                
 def getVerbosConjugados(listaVerbos):
     count = [1, 3, 2]
     contador = 0
@@ -113,10 +145,10 @@ def getVerbosConjugados(listaVerbos):
             soup = getConvertObjectSoup('http://www.conjuga-me.net/verbo-'+verbo, 'iso8859-1')
             
             for s in soup.find_all('td', attrs = { 'class' : 'output'}):
-                if s.text and s.text != None and s.text != '' and s.text != '\t' and s.text != '\n' and s.text != ' ':
+                if s.text and s.text != None and s.text != '' and s.text != '\t' and s.text != '\n' and s.text != ' ' and s.text != '–' and s.text != ' - ':
                     palavra = usarReplace(s.text.encode('utf-8'))
                     cursor.execute('select id_verbos from verbos where nome = "'+ verbo.strip() +'"')
-                    if palavra:
+                    if palavra and s.text != ' - ' and s.text != '   ':
                         id_verbo = cursor.fetchone()
                         print 'id_verbo: '+ str(id_verbo[0]) + ' - ' + palavra
                         sql = 'insert into conjulgacao_verbal (nome_conjulgacao, id_tempo_verbal, id_verbo) value (%s, %s, %s)'
@@ -139,16 +171,20 @@ def getVerbosConjugados(listaVerbos):
 
         cursor.close() 
     except Exception, e:
-        print 'Erro ao verificar verbos.' + e.message
+        erro = 'Erro ao executar getVerbosConjugados verbos: ' + e.message
+        enviaEmail(erro)
+        print erro
     else:
         pass
     finally:
-        pass		
+        pass                
 
 def main():
     verbos = getVerbos()
     verbosValidados = validaVerbos(verbos)
     getVerbosConjugados(verbosValidados)
+
+    enviaEmail('Todos os verbos foram inseridos com SUCESSO! - TESTE')
 
 if __name__ == '__main__':
     main()
