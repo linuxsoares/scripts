6ff4f84b548ea13ba2f57b301b499e3102f3e84c
linuxsoares <linux.soares@gmail.com>
2014-05-29 23:59:21 -0300

    Alterando loop for

--- getPessoasDesaparecidas.py
+++ getPessoasDesaparecidas.py
@@ -5,24 +5,18 @@ from bs4 import BeautifulSoup
 
 def getConvertObjectSoup(url, codin='utf-8'):
 	f = urllib.urlopen(url)
-
 	html_doc = f.read()
-
+	
 	return BeautifulSoup(html_doc, from_encoding=codin)
 
 def getPessoasDesaparecidas():
-	for i in range(1, 10):
+	for i in range(0, 11):
 		url = 'http://www.desaparecidos.gov.br/index.php/desparecidos?pag='+ str(i)
-
 		soup = getConvertObjectSoup(url)
 
 		print soup.find_all('div',attrs = { 'class' : 'boxDesaparecidor'})
+		print i
 	
-	
-	
-	
-	
-
 def main():
 	getPessoasDesaparecidas()
 
