2a81f3f7139d44bd32e78c5f6f6ed65a7521779a
linuxsoares <linux.soares@gmail.com>
2014-05-29 23:39:42 -0300

    Adicionando getPessoasDesaparecidas.py

---
+++ getPessoasDesaparecidas.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+
+import urllib
+from bs4 import BeautifulSoup
+
+def getConvertObjectSoup(url, codin='utf-8'):
+	f = urllib.urlopen(url)
+
+	html_doc = f.read()
+
+	return BeautifulSoup(html_doc, from_encoding=codin)
+
+def getPessoasDesaparecidas():
+	for i in range(1, 10):
+		url = 'http://www.desaparecidos.gov.br/index.php/desparecidos?pag='+ str(i)
+
+		soup = getConvertObjectSoup(url)
+
+		print soup.find_all('div',attrs = { 'class' : 'boxDesaparecidor'})
+	
+	
+	
+	
+	
+
+def main():
+	getPessoasDesaparecidas()
+
+if __name__ == '__main__':
+	main()
